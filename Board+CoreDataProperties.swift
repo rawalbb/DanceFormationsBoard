b69b79cb9c961e30cf1fd4b89467677cd8d74cc3
Bansri <Bansri.Rawal@ibm.com>
2021-01-15 22:12:37 -0500

    Reviewed Board+Properties

--- Board+CoreDataProperties.swift
+++ Board+CoreDataProperties.swift
@@ -11,11 +11,11 @@ import CoreData
 
 
 extension Board {
-
+    
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Board> {
         return NSFetchRequest<Board>(entityName: "Board")
     }
-
+    
     @NSManaged public var image: Data?
     @NSManaged public var lastEdited: Date
     @NSManaged public var name: String?
@@ -23,29 +23,27 @@ extension Board {
     @NSManaged public var song: String?
     @NSManaged public var uniqueId: String
     @NSManaged public var subFormations: NSSet?
-    //@NSManaged  var subFormations: Set<Formation>?
-
-    //@NSManaged var photos: Set<Photo>
-
+    
+    
 }
 
 // MARK: Generated accessors for subFormations
 extension Board {
-
+    
     @objc(addSubFormationsObject:)
     @NSManaged func addToSubFormations(_ value: Formation)
-
+    
     @objc(removeSubFormationsObject:)
     @NSManaged func removeFromSubFormations(_ value: Formation)
-
+    
     @objc(addSubFormations:)
     @NSManaged public func addToSubFormations(_ values: NSSet)
-
+    
     @objc(removeSubFormations:)
     @NSManaged public func removeFromSubFormations(_ values: NSSet)
-
+    
 }
 
 extension Board : Identifiable {
-
+    
 }
